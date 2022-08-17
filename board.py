bdfb0f8cb200cf6a15a4e68c30e40c9374accf25
samalthom <lalt20@bath.ac.uk>
2022-08-17 03:32:33 +0100

    board array

--- board.py
+++ board.py
@@ -1,12 +1,8 @@
 from pieces import Piece
+import numpy as np
 
 class Board(Piece):
     def __init__(self, size=3, first="c"):
         self.size = size
         self.first = first
-        if first == "c":
-            self.crosses = 5
-            self.noughts = 4
-        else:
-            self.noughts = 5
-            self.crosses = 4
+        self.board = np.zeroes(3,3)
