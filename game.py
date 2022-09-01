b6a614b53c48adaf2bd519f91a99cebdc973ce09
Samuel <samalthomson@gmail.com>
2022-09-01 02:00:50 +0100

    winner function

--- game.py
+++ game.py
@@ -20,4 +20,15 @@ class Game():
         return self.board
     
     def winner(self):
+        for i in range(3):
+            if self.board[i,0] == self.board[i,1] == self.board[i,2] and self.board != " ":
+                return self.board[i,0]
+            if self.board[0,i] == self.board[1,i] == self.board[2,i] and self.board != " ":
+                return self.board[0,i]
+        if self.board[0,0] == self.board[1,1] == self.board[2,2] and self.board != " ":
+            return self.board[0,0]
+        if self.board[2,0] == self.board[1,1] == self.board[0,2] and self.board != " ":
+            return self.board[2,0]
+        if np.all(self.board != " "):
+            return "d"
         return False
