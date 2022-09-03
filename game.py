8e1926e6b1d7a1244d74a17bd5563445b6e9d39b
Samuel <samalthomson@gmail.com>
2022-09-03 03:51:01 +0100

    Two player noughts and crosses complete

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class Game():
+class Game:
     def __init__(self):
         self.cross = "X"
         self.nought = "O"
@@ -11,36 +11,36 @@ class Game():
         self.swap_player = {self.cross: self.nought, self.nought: self.cross}
 
     def valid(self, row, col):
-        return self.board[row, col] == " "
+        return self.board[int(row), int(col)] == " "
 
     def move(self, move):
         row = move[0]
         col = move[1]
         if self.valid(row, col):
             self.board[row, col] = self.next_player
+            self.next_player = self.swap_player[self.next_player]
         return self.board
 
     def winner(self):
         for i in range(3):
-            if self.board[i, 0] == self.board[i, 1] == self.board[i, 2] and self.board != " ":
+            if self.board[i, 0] == self.board[i, 1] == self.board[i, 2] and self.board[i, 0] != " ":
                 return self.board[i, 0]
-            if self.board[0, i] == self.board[1, i] == self.board[2, i] and self.board != " ":
+            if self.board[0, i] == self.board[1, i] == self.board[2, i] and self.board[0, i] != " ":
                 return self.board[0, i]
-        if self.board[0, 0] == self.board[1, 1] == self.board[2, 2] and self.board != " ":
+        if self.board[0, 0] == self.board[1, 1] == self.board[2, 2] and self.board[0, 0] != " ":
             return self.board[0, 0]
-        if self.board[2, 0] == self.board[1, 1] == self.board[0, 2] and self.board != " ":
+        if self.board[2, 0] == self.board[1, 1] == self.board[0, 2] and self.board[2, 0] != " ":
             return self.board[2, 0]
         if np.all(self.board != " "):
             return "d"
         return False
 
 
-class Human():
+class Human:
     def next_move(self, state):
         while True:
             try:
-
-                move = input("What is your next move?")
+                move = input("What is your next move?: ")
                 row = move[0]
                 col = move[2]
                 move = [int(row), int(col)]
--- main.py
+++ main.py
@@ -2,38 +2,33 @@ from game import Game
 from game import Human
 
 
-
 def run_game(player1=Human(), player2=Human()):
     state = Game()
     print(state.board)
     while not state.winner():
+        print(" ")
+        print("Cross")
         move = player1.next_move(state)
         state.move(move)
         print(state.board)
-        while not state.winner():
-            move = player1.next_move(state)
-            state = state.move(move)
-            print(state.board)
-            if state.winner() == state.cross:
-                print("Player 1 wins!")
-                return
-            elif state.winner() == "d":
-                print("It's a draw.")
-                return
+        if state.winner() == state.cross:
+            print("Player 1 wins!")
+            return
+        elif state.winner() == "d":
+            print("It's a draw.")
+            return
 
+        print(" ")
+        print("Nought")
         move = player2.next_move(state)
         state.move(move)
         print(state.board)
-        while not state.winner():
-            move = player1.next_move(state)
-            state = state.move(move)
-            print(state.board)
-            if state.winner() == state.cross:
-                print("Player 2 wins!")
-                return
-            elif state.winner() == "d":
-                print("It's a draw.")
-                return
+        if state.winner() == state.cross:
+            print("Player 2 wins!")
+            return
+        elif state.winner() == "d":
+            print("It's a draw.")
+            return
 
 
 run_game()
